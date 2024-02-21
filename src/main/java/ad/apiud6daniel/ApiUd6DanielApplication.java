6f527d793a32d98a9634b5b09c5900662c669430
Daniel Gonzalez <daniel.gonzalez.losada@iessanmamede.com>
2024-02-21 15:18:26 +0100

    Inicio :)

---
+++ src/main/java/ad/apiud6daniel/ApiUd6DanielApplication.java
@@ -0,0 +1,13 @@
+package ad.apiud6daniel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUd6DanielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUd6DanielApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ad/apiud6daniel/ApiUd6DanielApplicationTests.java
@@ -0,0 +1,13 @@
+package ad.apiud6daniel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUd6DanielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
